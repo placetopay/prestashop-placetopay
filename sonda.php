2144c5a60b14db7dac594c65e15459c369228e05
Simón Alberto Godoy González <simon.godoy@placetopay.com>
2020-09-14 08:21:15 -0500

    Added: kernel in cronjob.

--- sonda.php
+++ sonda.php
@@ -13,6 +13,14 @@ try {
     require fixPath($pathCMS . '/config/config.inc.php');
     require fixPath(sprintf('%s/%2$s/%2$s.php', _PS_MODULE_DIR_, getModuleName()));
 
+    global $kernel;
+
+    if(!$kernel){
+        require_once _PS_ROOT_DIR_.'/app/AppKernel.php';
+        $kernel = new \AppKernel('prod', false);
+        $kernel->boot();
+    }
+
     (new PlacetoPayPayment())->resolvePendingPayments();
 } catch (Exception $e) {
     PaymentLogger::log($e->getMessage(), PaymentLogger::ERROR, 999, __FILE__, __LINE__);
