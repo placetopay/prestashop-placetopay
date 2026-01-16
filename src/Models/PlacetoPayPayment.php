6896bb78acdd221403663bb140a78d245d5d85fb
Freddie Gar <freddie.gar@outlook.com>
2026-01-15 23:43:02 -0500

    feat: support sha256 signatures in payment notifications

--- src/Models/PlacetoPayPayment.php
+++ src/Models/PlacetoPayPayment.php
@@ -125,9 +125,7 @@ class PlacetoPayPayment extends PaymentModule
 
     public function __construct()
     {
-        if (empty($this->name)) {
-            $this->name = getModuleName();
-        }
+        $this->name = getModuleName();
         $this->version = '5.0.0';
 
         $this->tab = 'payments_gateways';
@@ -179,10 +177,6 @@ class PlacetoPayPayment extends PaymentModule
 
     public function install()
     {
-        if (empty($this->name)) {
-            $this->name = getModuleName();
-        }
-        
         if (!parent::install()) {
             return false;
         }
@@ -762,13 +756,30 @@ class PlacetoPayPayment extends PaymentModule
         } elseif (!empty($inputStream = Tools::file_get_contents("php://input"))) {
             // On resolve function called process
             sleep(5);
-            $input = json_decode($inputStream, 1);
 
-            $notification = new Notification((array)$input, $this->getTranKey());
+            $input = json_decode($inputStream, true);
 
-            if (!$notification->isValidNotification()) {
+            $expectedSignature = sprintf(
+                '%s%s%s%s',
+                $input['requestId'],
+                $input['status']['status'],
+                $input['status']['date'],
+                $this->getTranKey()
+            );
+
+            if (strpos($input['signature'], ':') === false) {
+                $input['signature'] = 'sha1:' . $input['signature'];
+            }
+
+            [$algo, $signature] = explode(':', $input['signature'], 2);
+
+            if (isDebugEnable()) {
+                PaymentLogger::log('Signature algorithm: ' . $algo, PaymentLogger::DEBUG, 0, __FILE__, __LINE__);
+            }
+
+            if ($hashSignature = hash($algo, $expectedSignature) !== $signature) {
                 if (isDebugEnable()) {
-                    die('Change signature value in your request to: ' . $notification->makeSignature());
+                    die('Change signature value in your request to: ' . $hashSignature);
                 }
 
                 $message = 'Notification is not valid, process canceled. Input request:' . PHP_EOL . print_r($input, true);
